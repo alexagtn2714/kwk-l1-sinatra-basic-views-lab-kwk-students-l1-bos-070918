a06fb881610aa5557ac9f47d1c8ec7a65c27a927
alexagtn2714 <alexagaton@hotmail.com>
2018-07-17 18:28:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,18 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
+    
+    
   end 
 
-  get '/name' do
-    "Buster's Best Bowling Alley"
-  end 
-  
-  get '/tagline' do 
-    "Bet You Can't Bowl A Strike Every Time"
-  end 
-  
-  get '/address' do 
-    "97 Bowling Alley Lane, Boulder CO"
-  end 
+
 
 end
